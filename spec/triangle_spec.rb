1b3356a89670da385cbe86cabe3f4419974cd16c
Guest User <student@epicodus.com>
2015-04-27 15:06:51 -0700

    Adds spec for isosceles triangle

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -7,6 +7,11 @@ describe('Triangle') do
   describe('#triangle') do
   it('checks a triangle is equal on all sides') do
     expect(Triangle.new(3,3,3).triangle()).to(eq("It's a equilateral triangle."))
+    end
   end
-end
+  describe('#triangle') do
+  it('checks a triangle is equal has two equal sides and diffrent for third side') do
+    expect(Triangle.new(4,3,3).triangle()).to(eq("It's a isosceles triangle."))
+    end
+  end
 end
