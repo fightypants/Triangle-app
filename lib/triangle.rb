16065a78986b05d201e729e5c8c0fb42e91cc60d
Guest User <student@epicodus.com>
2015-04-27 15:03:55 -0700

    It passes the equalateral triangle spec

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,13 @@
 class Triangle
+  define_method(:initialize) do |width, height, length|
+    @width = width
+    @height = height
+    @length = length
+  end
+
   define_method(:triangle) do
+    if @width == @height && @width == @length
+      "It's a equilateral triangle."
+    end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -6,7 +6,7 @@ require ('rspec')
 describe('Triangle') do
   describe('#triangle') do
   it('checks a triangle is equal on all sides') do
-    expect(3,3,3.triangle()).to(eq(equilateral))
+    expect(Triangle.new(3,3,3).triangle()).to(eq("It's a equilateral triangle."))
   end
 end
 end
